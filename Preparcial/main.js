00d5dd41f7c9dafd13f1df2befcf0bda2560c8a9
alejoalcaraz <a.alcaraz@uniandes.edu.co>
2022-03-16 23:56:49 -0500

    Parte 2 parcial 1

--- Preparcial/main.js
+++ Preparcial/main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-tabs */
 {
-  const actual = 0;
-  const url = 'https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json';
+  let actual = 0;
+  const url = 'datos.json';
+  let datos = [];
+  const productos = [];
   /**
    *  funcion de lectura
    * @param {string} url
@@ -25,32 +27,102 @@
   };
 
   lectura(url).then((result)=>{
-    const datos = JSON.parse(result);
+    console.log('gas');
+    datos = JSON.parse(result);
     console.log(datos);
+    cambiarPag(0);
+  });
 
+  /**
+   *
+   * @param {int} n
+   */
+  function cambiarPag(n) {
+    actual = n;
+    let nombre = ' ';
+    console.log(datos[n].name);
+    nombre = document.getElementById('titleFood');
+    nombre.innerHTML = datos[n].name;
+
+    for (let i = 0; i < 4; i++) {
+      nombreP = document.getElementById('nombreProducto'+i);
+      nombreP.innerHTML = datos[n].products[i].name;
+      foto = document.getElementById('imgProducto'+i);
+      foto.src = datos[n].products[i].image;
+      descripcion = document.getElementById('descripcionProducto'+i);
+      descripcion.innerHTML = datos[n].products[i].description;
+      precio = document.getElementById('precioProducto'+i);
+      precio.innerHTML = datos[n].products[i].price;
+    }
+  };
+  let anadidos = 0;
+  /**
+   *
+   * @param {int} n
+   */
+  function anadir(n) {
+    anadidos += 1;
+    document.getElementById('contObjetos').innerHTML = anadidos + '  items';
+
+    productoAnadir = datos[actual].products[n];
+    productos.push(productoAnadir);
+    console.log(productos);
+  };
+  /**
+   *
+   */
+  function tablaCompra() {
+    header = `<div class="container">
+    <table class="table table-striped border-success">
+      <tr class="bg-info">
+        <th>Item</th>
+        <th>Qty</th>
+        <th>Description</th>
+        <th>Unit price</th>
+        <th>Amount</th>
+        <th>Modify</th>
+      </tr>
+
+      <tbody id="tablaEventos">
+
+      </tbody>
+    </table>
+    <div class="row">
+    <div class="col-2">
+        <p>
+            total :
+        </p>
+    </div>
+    <div class="col-6">
+
+    </div>
+    <div class="col-2">
+        <button type="button">cancel</button>
+    </div>
+    <div class="col-2">
+        <button type="button">confirm</button>
+    </div>
+</div>
+  </div>`;
+    tabla = document.getElementById('tabla');
+    tabla.innerHTML = header;
 
     const table = document.getElementById('tablaEventos');
 
-    for (let i = 0; i < datos.length; i++) {
-      let row;
-      // eslint-disable-next-line prefer-const
-      row = `<tr>
-							<td>${datos[i].last_name}</td>
-              <td>${datos[i].first_name}</td>
-              <td>${datos[i].email}</td>
-              <td><img src = "${datos[i].photo}"></td>
-              <td><input type="button" value="Delete Row" 
-              onclick="DeleteRowFunction()"></td>
-					    </tr>`;
-
+    console.log(productos.length);
+    for (let i = 0; i < productos.length; i++) {
+      const row = `<tr>
+    					<td>${i+1}</td>
+              <td>${1}</td>
+              <td>${productos[i].name}</td>
+              <td>${productos[i].price}</td>
+              <td>${productos[i].price}</td>
+              <td>
+              <button type="button" class="btnMod" onClick="mod()">+</button>
+              <button type="button" class="btnMod" onClick="mod()">-</button>
+              </td>
+    			    </tr>`;
       table.innerHTML += row;
-    }
-  });
-
-  function cambiarPag(n) {
-    actual = n;
-    if (actual== 1) {
-
-    }
+    };
   }
 }
